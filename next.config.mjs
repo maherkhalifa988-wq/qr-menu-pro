5ab78ec43aadcf8d8f0ad7699e71b25bd4481773
maherkhalifa988-wq <maherkhalifa988@gmail.com>
2025-09-24 12:07:10 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'res.cloudinary.com' },
-    ],
+    domains: ['res.cloudinary.com'],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
